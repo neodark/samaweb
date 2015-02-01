8817650dcf3f6bc0daad9b9e6ddae8568a19e83c
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-02-02 00:00:32 +0100

    [samacore]cleaned views.py

--- samacore/views.py
+++ samacore/views.py
@@ -15,15 +15,9 @@ def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     param_1 = 2
     context = {'params': param_1}
-    print "index"
     return render(request, 'samacore/index.html', context)
 
 def section(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     param_1 = 2
     context = {'params': param_1}
-    #return render(request, 'core/section.html', context)
-    #return render(request, 'core/section.html')
-    #return render_to_response('samacore/section.html', response, context_instance=RequestContext(request))
-    print "ici"
     return render(request, 'samacore/section.html', context)
