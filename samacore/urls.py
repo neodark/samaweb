785b378e0f9fbca1856b548c2caa548b83be1154
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-02-01 23:38:58 +0100

    [samacore] adding app samacore and updated urls/views

---
+++ samacore/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from . import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'samaweb.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^section/$', views.section, name='section'),
+)
