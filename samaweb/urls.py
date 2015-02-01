e3b4add60ba28bb2d9b850fe14670f63f4bd8d9d
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-02-01 23:42:50 +0100

    [samaweb] updated urls.py by including samacore.urls

--- samaweb/urls.py
+++ samaweb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from samacore import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('core.urls')),
+    url(r'^', include('samacore.urls')),
+    #url(r'^section/$', samacore.views.section, name='section'),
 )
+urlpatterns += staticfiles_urlpatterns()
