88732d9e158beeb2056d76c6daf01bd5237b85b2
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-02-24 00:15:24 +0100

    [api] added first unit-test with tests.py file

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIRequestFactory, APIClient
+#Create your tests here.
+
+class TestAPITestClient(TestCase):
+    #urls = 'api.tests.test_testing'
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_credentials(self):
+        """
+        Setting '.credentials()' adds the required headers to each request.
+        """
+        self.assertEqual(2,2)
