3745ca9f1b79535f3ae422133d29c606aeee3783
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-03-02 00:41:11 +0100

    [api] implemented api classed based views for CourseType

--- api/views.py
+++ api/views.py
@@ -7,8 +7,8 @@ from rest_framework.response import Response
 from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView )
 
-from samacore.models import SamaMember
-from api.serializers import SamaMemberSerializer
+from samacore.models import SamaMember, CourseType
+from api.serializers import SamaMemberSerializer, CourseTypeSerializer
 
 # Create your views here.
 
@@ -30,6 +30,27 @@ class SamaMemberList(SamaMemberMixin, ListCreateAPIView):
     pass
 
 class SamaMemberDetail(SamaMemberMixin, RetrieveUpdateDestroyAPIView):
+    """
+    Return a specific SamaMember, update it, or delete it.
+    """
+    pass
+
+class CourseTypeMixin(object):
+    """
+    Mixin to inherit from.
+    Here we're setting the query set and the serializer
+    """
+    queryset = CourseType.objects.all()
+    serializer_class = CourseTypeSerializer
+
+class CourseTypeList(CourseTypeMixin, ListCreateAPIView):
+    """
+    Return a list of all the SamaMembers, or
+    create new ones
+    """
+    pass
+
+class CourseTypeDetail(CourseTypeMixin, RetrieveUpdateDestroyAPIView):
     """
     Return a specific SamaMember, update it, or delete it.
     """
