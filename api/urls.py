4d209440c29486b9a2fb181be585b83d61d0b5bc
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-03-02 00:43:14 +0100

    [api] added class based API url views for coursetype

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Class based API views
 from api.views import SamaMemberList, SamaMemberDetail
+from api.views import CourseTypeList, CourseTypeDetail
 
 urlpatterns = patterns('',
 
@@ -12,4 +13,6 @@ urlpatterns = patterns('',
     # Class based URLs,
     url( r'^samamembers/$', SamaMemberList.as_view(), name = 'samamember_list' ),
     url( r'^samamembers/(?P<pk>[0-9]+)$', SamaMemberDetail.as_view(), name = 'samamember_detail' ),
+    url( r'^coursetype/$', CourseTypeList.as_view(), name = 'coursetype_list' ),
+    url( r'^coursetype/(?P<pk>[0-9]+)$', CourseTypeDetail.as_view(), name = 'coursetype_detail' ),
 )
