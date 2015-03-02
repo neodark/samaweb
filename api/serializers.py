35b0e2c0bbae552149798e45d14968a70daca835
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-03-03 00:22:40 +0100

    [api] added model serializer for SamaGroup

--- api/serializers.py
+++ api/serializers.py
@@ -11,6 +11,18 @@ class SamaMemberSerializer( serializers.ModelSerializer ):
 		fields = ( 'first_name', 'last_name', 'birth_date', 'sex', 
                    'email', 'address', 'npa', 'city', 'phone', 'id' )
 
+class SamaGroupSerializer( serializers.ModelSerializer ):
+	"""
+	Serializer to parse SamaGroup data
+	"""
+
+	class Meta:
+		model = SamaGroup
+		fields = ( 'name', 'type', 'samamember', 'id' )
+        extra_kwargs = {
+            'samamember': {'required':False}
+        }
+
 class CourseTypeSerializer( serializers.ModelSerializer ):
 	"""
 	Serializer to parse CourseType data
