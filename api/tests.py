1f3f3a02560d3d07043dc902c4923eee25135ecf
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-04-11 00:59:58 +0200

    [api] updated api tests

--- api/tests.py
+++ api/tests.py
@@ -22,6 +22,7 @@ class TestAPITestSamaMembers(TestCase):
                      'npa':1234,\
                      'city':'matrixland',\
                      'phone':'123.145/34',\
+                     'samagroup':[],\
                      'id':1}
 
         self.updated_json_data = {\
@@ -34,6 +35,7 @@ class TestAPITestSamaMembers(TestCase):
                      'npa':5678,\
                      'city':'matrixlandupdated',\
                      'phone':'567.123/89',\
+                     'samagroup':[],\
                      'id':1}
 
     def test_simple_test(self):
@@ -137,12 +139,14 @@ class TestAPITestClientCourseType(TestCase):
         self.client = APIClient()
         self.json_data = {\
                      'name':'cours 1',\
-                     'type':1,\
+                     'course_identifier':1,\
+                     'courses':[],\
                      'id':1}
 
         self.updated_json_data = {\
                      'name':'cours 2',\
-                     'type':2,\
+                     'course_identifier':2,\
+                     'courses':[],\
                      'id':1}
 
     def test_simple_test(self):
@@ -236,5 +240,116 @@ class TestAPITestClientCourseType(TestCase):
         self.assertEqual(request3.status_code, status.HTTP_204_NO_CONTENT)
 
         request4 = self.client.get('/api/coursetype/', format='json')
+        self.assertEqual(request4.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json.loads(request4.content)), 0)
+
+class TestAPITestClientSamaGroup(TestCase):
+    #urls = 'api.tests.test_testing'
+
+    def setUp(self):
+        self.client = APIClient()
+        self.json_data = {\
+                     'name':'samagroup 1',\
+                     'sama_identifier':1,\
+                     'samamembers':[],\
+                     'id':1}
+
+        self.updated_json_data = {\
+                     'name':'samagroup 2',\
+                     'sama_identifier':2,\
+                     'samamembers':[],\
+                     'id':1}
+
+    def test_simple_test(self):
+        """
+        Simple test
+        """
+        self.assertEqual(2,2)
+
+    def test_get_empty_db(self):
+        """
+        Testing GET on an empty database
+        """
+        request = self.client.get('/api/samagroup/', {}, format='json')
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json.loads(request.content)), 0)
+
+    def test_post_db(self):
+        """
+        Testing a POST and GET on a database
+        """
+
+        request1 = self.client.post('/api/samagroup/', self.json_data, format='json')
+        self.assertEqual(request1.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(json.dumps(request1.data, sort_keys=True), json.dumps(OrderedDict(self.json_data), sort_keys=True))
+        self.assertEqual(json.dumps(json.loads(request1.content), sort_keys=True), json.dumps(self.json_data, sort_keys=True))
+
+        request2 = self.client.get('/api/samagroup/', format='json')
+        self.assertEqual(request2.status_code, status.HTTP_200_OK)
+        json_get = ""
+        for key in json.loads(request2.content):
+            json_get = json.dumps(key, sort_keys=True)
+
+        self.assertEqual(json_get, json.dumps(self.json_data, sort_keys=True))
+
+    def test_put_db(self):
+        """
+        Testing a POST and GET followed by a PUT and GET on a database
+        """
+
+        request1 = self.client.post('/api/samagroup/', self.json_data, format='json')
+        self.assertEqual(request1.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(json.dumps(request1.data, sort_keys=True), json.dumps(OrderedDict(self.json_data), sort_keys=True))
+        self.assertEqual(json.dumps(json.loads(request1.content), sort_keys=True), json.dumps(self.json_data, sort_keys=True))
+
+        request2 = self.client.get('/api/samagroup/', format='json')
+        self.assertEqual(request2.status_code, status.HTTP_200_OK)
+        json_get = ""
+        for key in json.loads(request2.content):
+            json_get = json.dumps(key, sort_keys=True)
+
+        self.assertEqual(json_get, json.dumps(self.json_data, sort_keys=True))
+
+
+        request3 = self.client.put('/api/samagroup/1', self.updated_json_data, format='json')
+        self.assertEqual(request3.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(json.dumps(request3.data, sort_keys=True), json.dumps(OrderedDict(self.updated_json_data), sort_keys=True))
+        self.assertEqual(json.dumps(json.loads(request3.content), sort_keys=True), json.dumps(self.updated_json_data, sort_keys=True))
+
+        request4 = self.client.get('/api/samagroup/', format='json')
+        self.assertEqual(request4.status_code, status.HTTP_200_OK)
+        json_get = ""
+        for key in json.loads(request4.content):
+            json_get = json.dumps(key, sort_keys=True)
+
+        self.assertEqual(json_get, json.dumps(self.updated_json_data, sort_keys=True))
+
+    def test_delete_db(self):
+        """
+        Testing a POST and GET followed by a DELETE and GET on a database
+        """
+
+        request1 = self.client.post('/api/samagroup/', self.json_data, format='json')
+        self.assertEqual(request1.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(json.dumps(request1.data, sort_keys=True), json.dumps(OrderedDict(self.json_data), sort_keys=True))
+        self.assertEqual(json.dumps(json.loads(request1.content), sort_keys=True), json.dumps(self.json_data, sort_keys=True))
+
+        request2 = self.client.get('/api/samagroup/', format='json')
+        self.assertEqual(request2.status_code, status.HTTP_200_OK)
+        json_get = ""
+        for key in json.loads(request2.content):
+            json_get = json.dumps(key, sort_keys=True)
+
+        self.assertEqual(json_get, json.dumps(self.json_data, sort_keys=True))
+
+
+        request3 = self.client.delete('/api/samagroup/1', self.updated_json_data, format='json')
+        self.assertEqual(request3.status_code, status.HTTP_204_NO_CONTENT)
+
+        request4 = self.client.get('/api/samagroup/', format='json')
         self.assertEqual(request4.status_code, status.HTTP_200_OK)
         self.assertEqual(len(json.loads(request4.content)), 0)
