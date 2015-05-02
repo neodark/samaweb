991acd5b676781418f0c6c5fa012d652884a08dd
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-05-02 17:41:27 +0200

    [samaweb][heroku] patched settings.py for deployment

--- samaweb/settings.py
+++ samaweb/settings.py
@@ -69,12 +69,12 @@ WSGI_APPLICATION = 'samaweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] =  dj_database_url.config()
