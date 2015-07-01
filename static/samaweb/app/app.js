df7d78c7169aa54e4f064775b684c1b2c26c4fcc
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-07-02 00:19:46 +0200

    [static][samaweb][app] added route/state providers to app.js

--- static/samaweb/app/app.js
+++ static/samaweb/app/app.js
@@ -1,1 +1,19 @@
-﻿var app = angular.module('samawebApp', []);
+var app = angular.module('samawebApp', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider
+        .otherwise('/');
+
+    $stateProvider
+        .state('course', {
+            url: '/',
+            views: {
+                'courseInfo': {
+                    templateUrl: '/partials/courseInfo/',
+                    //templateUrl: '/reports/partials/reportGeneralInfo.html',
+                    controller: 'courseController',
+                }
+            }
+        })
+});
