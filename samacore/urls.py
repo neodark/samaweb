6eb2120d1484a3aeae77ef1c5c96f105d876693c
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-07-02 00:09:15 +0200

    [samacore] updated urls to use views.PartialGroupView to load partials

--- samacore/urls.py
+++ samacore/urls.py
@@ -1,15 +1,27 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
-urlpatterns = patterns('',
+
+partial_patterns = [
+    url(
+        r'^courseInfo/$',
+        views.PartialGroupView.as_view(template_name='samacore/partials/courseInfo.html'),
+        name='course_info',
+        ),
+    url(r'^(?P<template_name>[a-zA-Z_]+\.html)$', views.PartialGroupView.as_view()),
+]
+
+
+urlpatterns = [
     # Examples:
     # url(r'^$', 'samaweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^partials/', include(partial_patterns, namespace='partials')),
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
     url(r'^section/$', views.section, name='section'),
     url(r'^cours/$', views.courses, name='courses'),
     url(r'^specific_cours/$', views.course, name='course'),
     url(r'^faq/$', views.faq, name='faq'),
-)
+]
