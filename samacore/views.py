200bdb28d19f7b8f33ff35b7c0b0d347070820a3
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-07-02 00:08:00 +0200

    [samacore] added PartialGroupView in views.py to load partials

--- samacore/views.py
+++ samacore/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404
 
+from django.views.generic import TemplateView
 # Create your views here.
 
 #from django.http import HttpResponse
@@ -39,3 +40,19 @@ def course(request):
 
     context = {'coursetype': coursetype}
     return render(request, 'samacore/course.html', context)
+
+#------------------------------------------------
+
+
+class PartialGroupView(TemplateView):
+    def get_template_names(self):
+        if 'template_name' in self.kwargs:
+            self.template_name = 'samacore/partials/' + self.kwargs.get('template_name')
+        return super(PartialGroupView, self).get_template_names()
+
+    def get_context_data(self, **kwargs):
+        context = super(PartialGroupView, self).get_context_data(**kwargs)
+        return context
+
+
+#------------------------------------------------
