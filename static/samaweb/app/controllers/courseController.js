a40b296670b22f4ef4e5a0f47feb155e35e8a487
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-07-02 00:48:10 +0200

    [static][samaweb][app][controllers] updated courseController with django urls in initialization

--- static/samaweb/app/controllers/courseController.js
+++ static/samaweb/app/controllers/courseController.js
@@ -5,21 +5,27 @@ app.controller('courseController',['$scope', 'courseFactory', function ($scope, 
    $scope.courses;
    $scope.course_type;
    $scope.course_list_url;
+   $scope.url_prefix;
+   $scope.static_url;
 
-   $scope.init = function(course_type, course_list_url)
+   $scope.init = function(course_type, course_list_url, url_prefix, static_url)
    {
        $scope.course_type     = course_type;
        $scope.course_list_url = course_list_url;
+       $scope.url_prefix = url_prefix;
+       $scope.static_url = static_url;
        getCourseData($scope.course_type, $scope.course_list_url);
    }
  
-    function getCourseData(course_type, course_list_url) {
-        courseFactory.getCoursesInformation(course_type, course_list_url)
-            .success(function (coursesData) {
-                $scope.courses = coursesData;
-            })
-            .error(function (error) {
-                $scope.status = 'Unable to load courses data: ' + error.message;
-            });
-    }
+   function getCourseData(course_type, course_list_url)
+   {
+       courseFactory.getCoursesInformation(course_type, course_list_url)
+           .success(function (coursesData) {
+               $scope.courses = coursesData;
+               $scope.courses.url_prefix = $scope.url_prefix;
+           })
+           .error(function (error) {
+               $scope.status = 'Unable to load courses data: ' + error.message;
+           });
+   }
 }]);
