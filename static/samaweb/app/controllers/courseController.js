02105b4d12b2fb4e9e5241532ea2a047ecc2e625
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-07-08 20:28:23 +0200

    [samaweb][app][controllers] removed passing url_prefix/static_url to courseController

--- static/samaweb/app/controllers/courseController.js
+++ static/samaweb/app/controllers/courseController.js
@@ -5,15 +5,11 @@ app.controller('courseController',['$scope', 'courseFactory', function ($scope, 
    $scope.courses;
    $scope.course_type;
    $scope.course_list_url;
-   $scope.url_prefix;
-   $scope.static_url;
 
-   $scope.init = function(course_type, course_list_url, url_prefix, static_url)
+   $scope.init = function(course_type, course_list_url)
    {
        $scope.course_type     = course_type;
        $scope.course_list_url = course_list_url;
-       $scope.url_prefix = url_prefix;
-       $scope.static_url = static_url;
        getCourseData($scope.course_type, $scope.course_list_url);
    }
  
@@ -22,7 +18,6 @@ app.controller('courseController',['$scope', 'courseFactory', function ($scope, 
        courseFactory.getCoursesInformation(course_type, course_list_url)
            .success(function (coursesData) {
                $scope.courses = coursesData;
-               $scope.courses.url_prefix = $scope.url_prefix;
            })
            .error(function (error) {
                $scope.status = 'Unable to load courses data: ' + error.message;
