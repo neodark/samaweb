9efcbc1b99aca44718cbfe8633338e55e623f00b
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-10-30 09:04:48 +0100

    [api][templates] adding admin login (work in progress ...)

--- api/serializers.py
+++ api/serializers.py
@@ -68,3 +68,20 @@ class CourseTypeSerializer( serializers.ModelSerializer ):
     class Meta:
         model = CourseType
         fields = ( 'name', 'course_identifier', 'id', 'courses' )
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('password', 'first_name', 'last_name', 'email',)
+        write_only_fields = ('password',)
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
+
+    def restore_object(self, attrs, instance=None):
+        # call set_password on user object. Without this
+        # the password will be stored in plain text.
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
