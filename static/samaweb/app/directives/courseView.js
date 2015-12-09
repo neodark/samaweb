c2b505360df746921ac1274b0aeec6670849e113
Flavio Tarsetti <tarsetti.flavio@gmail.com>
2015-12-09 09:23:29 +0100

    [app] code cleaning: removed unused debug information in courseView.js

--- static/samaweb/app/directives/courseView.js
+++ static/samaweb/app/directives/courseView.js
@@ -4,15 +4,11 @@ var app = angular.module('samawebApp');
 //Directive to login on button click
 app.directive("buttonsavecourse", function($compile)
 {
-    console.log("hello");
 	return function(scope, element, attrs)
     {
         //login button click
 		element.bind("click", function()
         {
-            console.log(scope);
-            console.log(element);
-            console.log(attrs);
             scope.save_course(scope.course_type, scope.new_course_dates, scope.new_course_address, scope.new_course_maximum_participants);
 		});
     }
